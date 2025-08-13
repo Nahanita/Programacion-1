e70c630fb570dad15146a913e3dfb6d17b5eaa74
Nahanita <saranahany089@gmail.com>
2025-08-13 17:14:39 -0500

    Add files via upload

---
+++ holi/index.php
@@ -0,0 +1,18 @@
+<?php
+$num1 = 5;
+$num2 = 3;
+echo "Holi mundo";
+echo "<br>";
+echo "la suma es " . (2+5);
+echo "<br>";
+echo "la resta es " . (8-2);
+echo "<br>";
+echo "la multiplicación es " . (5*5);
+echo "<br>";
+echo "la división es " . (15/5);
+echo "<br>";
+echo "la suma es " . ($num1+$num2);
+echo "<br>";
+echo "la resta es " . ($num1-$num2);
+
+?>
